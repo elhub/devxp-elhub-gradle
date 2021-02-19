8fc192ecc85698c7703e367cb887b97b41bd84b9
Michael Akinde <michael.akinde@statnett.no>
2021-02-19 09:37:31 +0100

    Fix the TeamCity settings
    Test Plan: mvn compile in the .teamcity directory
    
    Reviewers: O5 elhub/devtools, sergei.prodanov
    
    Reviewed By: O5 elhub/devtools, sergei.prodanov
    
    JIRA Issues: TD-83
    
    Differential Revision: https://phabricator.elhub.cloud/D1316

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,16 +1,11 @@
-import jetbrains.buildServer.configs.kotlin.v2019_2.version
-import jetbrains.buildServer.configs.kotlin.v2019_2.project
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
-import jetbrains.buildServer.configs.kotlin.v2019_2.CheckoutMode
 import jetbrains.buildServer.configs.kotlin.v2019_2.DslContext
-import jetbrains.buildServer.configs.kotlin.v2019_2.ProjectFeature
-import jetbrains.buildServer.configs.kotlin.v2019_2.ProjectFeatures
-import jetbrains.buildServer.configs.kotlin.v2019_2.Template
-import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.VcsTrigger
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.v2019_2.project
+import jetbrains.buildServer.configs.kotlin.v2019_2.sequential
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
-import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
-import no.elhub.common.build.configuration.CreateExeGradle
-import no.elhub.common.build.configuration.UnitTestGradle
+import jetbrains.buildServer.configs.kotlin.v2019_2.version
+import no.elhub.common.build.configuration.SonarScan
 
 val projectId = "dev-tools-elhub-gradle"
 version = "2020.2"
@@ -35,44 +30,41 @@ project {
             )
         )
 
-        buildType(
-            object Build : BuildType({
-                id = "Assemble"
-                name = "Assemble"
+        buildType(BuildType {
+            name = "Assemble"
 
+            vcs {
+                root(DslContext.settingsRoot)
+            }
+
+            steps {
+                gradle {
+                    name = "Assemble jar file"
+                    tasks = "buildDist"
+                    buildFile = "build.gradle.kts"
+                }
+                gradle {
+                    name = "Publish to Artifactory"
+                    tasks = "artifactoryPublish"
+                    buildFile = "build.gradle.kts"
+                    param("org.jfrog.artifactory.selectedDeployableServer.projectUsesArtifactoryGradlePlugin", "true")
+                    param("org.jfrog.artifactory.selectedDeployableServer.useM2CompatiblePatterns", "false")
+                    param("org.jfrog.artifactory.selectedDeployableServer.publishBuildInfo", "true")
+                    param("org.jfrog.artifactory.selectedDeployableServer.defaultModuleVersionConfiguration", "GLOBAL")
+                    param("org.jfrog.artifactory.selectedDeployableServer.urlId", "0")
+                    param("org.jfrog.artifactory.selectedDeployableServer.envVarsExcludePatterns", "*password*,*secret*")
+                    param("org.jfrog.artifactory.selectedDeployableServer.publishMavenDescriptors", "true")
+                    param("org.jfrog.artifactory.selectedDeployableServer.deployArtifacts", "true")
+                    param("org.jfrog.artifactory.selectedDeployableServer.activateGradleIntegration", "true")
+                    param("org.jfrog.artifactory.selectedDeployableServer.targetRepo", "elhub-bin-release-local")
+                }
+            }
+
+            triggers {
                 vcs {
-                    root(DslContext.settingsRoot)
                 }
-
-                steps {
-                    gradle {
-                        name = "Assemble jar file"
-                        tasks = "buildDist"
-                        buildFile = "build.gradle.kts"
-                    }
-                    gradle {
-                        name = "Publish to Artifactory"
-                        tasks = "artifactoryPublish"
-                        buildFile = "build.gradle.kts"
-                        param("org.jfrog.artifactory.selectedDeployableServer.projectUsesArtifactoryGradlePlugin", "true")
-                        param("org.jfrog.artifactory.selectedDeployableServer.useM2CompatiblePatterns", "false")
-                        param("org.jfrog.artifactory.selectedDeployableServer.publishBuildInfo", "true")
-                        param("org.jfrog.artifactory.selectedDeployableServer.defaultModuleVersionConfiguration", "GLOBAL")
-                        param("org.jfrog.artifactory.selectedDeployableServer.urlId", "0")
-                        param("org.jfrog.artifactory.selectedDeployableServer.envVarsExcludePatterns", "*password*,*secret*")
-                        param("org.jfrog.artifactory.selectedDeployableServer.publishMavenDescriptors", "true")
-                        param("org.jfrog.artifactory.selectedDeployableServer.deployArtifacts", "true")
-                        param("org.jfrog.artifactory.selectedDeployableServer.activateGradleIntegration", "true")
-                        param("org.jfrog.artifactory.selectedDeployableServer.targetRepo", "elhub-bin-release-local")
-                    }
-                }
-
-                triggers {
-                    vcs {
-                    }
-                }
-            })
-        )
+            }
+        })
 
     }
 
